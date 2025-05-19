13aed6112d82bc20d66cd067a2265e8bc1a2e237
amanghuman <58965636+amanghuman@users.noreply.github.com>
2025-05-19 09:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("styles.css")
+local_css("style.css")
 
 # Load Lottie animation
 def load_lottie_url(url):
