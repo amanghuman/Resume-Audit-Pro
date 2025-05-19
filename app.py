bf12df88bc4e06f7a3f3749227c1c1233a6df892
amanghuman <58965636+amanghuman@users.noreply.github.com>
2025-05-19 09:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import requests
 
 # Load environment variables
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets.gemini.api_key)
 
 st.set_page_config(
     page_title="Resume Audit Pro",
